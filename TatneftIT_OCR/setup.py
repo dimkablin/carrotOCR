f0375139297f47f6c236f0a328ca91fb675da510
dimka <dmitryellison@gmail.com>
2023-09-12 23:28:47 +0300

    Initial commit

---
+++ TatneftIT_OCR/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementation of OCR from open sources for internal tasks of the Tatneft Group.',
+    author='TatneftIT',
+    license='MIT',
+)
