25cf78194105319afcd44982f189a169ef7ea0b0
dimka <dmitryellison@gmail.com>
2023-09-22 13:26:44 +0300

    added ocr_interface.py

--- models/ocr_interface.py
+++ models/ocr_interface.py
@@ -7,7 +7,14 @@ class OCR(ABC):
     """ Interface of ocr models """
     @abstractmethod
     def __call__(self, *args, **kwargs) -> dict:
-        pass
+        """
+        Perform OCR on an image.
+
+        Returns:
+            dict: A dictionary containing the OCR result.
+                  - 'bbox': tuple representing bounding box coordinates
+                  - 'text': string representing the recognized text
+        """
 
     @abstractmethod
     def __str__(self) -> str:
