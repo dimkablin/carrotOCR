7531faaf7591e411aeca420a954f33f69d8efe30
dimka <dmitryellison@gmail.com>
2023-10-05 00:19:44 +0300

    Remove .env file from tracking

---
+++ src/db/database_processor.py
@@ -0,0 +1,19 @@
+""" Data Processor Package """
+import psycopg2
+from psycopg2 import sql
+from src.db.database_manager import DatabaseManager
+
+
+class DataProcessor:
+    """ Class to work with Database Manager """
+    @staticmethod
+    def insert_data():
+        pass
+
+    @staticmethod
+    def fetch_data():
+        pass
+
+    @staticmethod
+    def insert_new_name_of_file():
+        pass
