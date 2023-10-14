0dce8af80986c33dd704c2fa66d5be01cd82ed2a
dimka <dmitryellison@gmail.com>
2023-10-14 04:16:25 +0300

    get_folders_service

--- src/api/services/get_folders.py
+++ src/api/services/get_folders.py
@@ -1,10 +1,15 @@
-"""get_folders function controller."""
+"""get_folders function service."""
 import os
 from src.api.models.get_f import GetFRequest, GetFoldersResponse
 
 
 async def get_folders_service(req: GetFRequest) -> GetFoldersResponse:
-    """get_files function controller"""
+    """get_files function service"""
+    path, dirname = os.path.split(req.path)
+    folders = []
+    if os.path.exists(path):
+        for dir_ in os.listdir(path):
+            if os.path.isdir(os.path.join(path, dir_)) and dir_.startswith(dirname):
+                folders.append(dir_)
 
-    folders = [dir for dir in os.listdir(req.path) if os.path.isdir(os.path.join(req.path, dir))]
     return GetFoldersResponse(folders=folders[:req.count])
