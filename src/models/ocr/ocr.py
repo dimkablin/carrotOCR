52b47496cbbff172d070a6d771e0450dc7dd2ce2
dimka <dmitryellison@gmail.com>
2023-10-24 00:00:29 +0300

    DELETED MMOCR

--- src/models/ocr/ocr.py
+++ src/models/ocr/ocr.py
@@ -1,6 +1,5 @@
 """ Factory Method - Design Pattern """
 from src.models.ocr.easyocr import EasyOCRInited
-from src.models.ocr.mmocr import MMOCRInited
 from src.models.ocr.pytesseract import PyTesseractInited
 
 
@@ -8,7 +7,6 @@ class OCRModelFactory:
     """ Factory Method - Design Pattern implementation """
 
     MODEL_MAPPING = {
-        MMOCRInited.get_model_type(): MMOCRInited,
         PyTesseractInited.get_model_type(): PyTesseractInited,
         EasyOCRInited.get_model_type(): EasyOCRInited,
     }
