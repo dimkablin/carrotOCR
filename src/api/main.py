436a8c322083ee09f9f13977a383ce3b27e94b1b
Stepan <stepan.sharifulin@mail.ru>
2023-10-27 06:49:07 +0500

    Update cors

--- src/api/main.py
+++ src/api/main.py
@@ -42,7 +42,7 @@ router = APIRouter()
 app.add_middleware(BackendMiddleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True,
