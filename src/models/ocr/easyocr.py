1934b98434c14b355362adc103be85eff04f5197
dimka <dmitryellison@gmail.com>
2023-11-01 12:34:27 +0300

    fixed chunbk_id in process_image and get_processed
    but deleted generalpipeline

--- src/models/ocr/easyocr.py
+++ src/models/ocr/easyocr.py
@@ -17,7 +17,7 @@ class EasyOCRInited(OCR):
     def __call__(self, inputs, *args, **kwargs) -> list[dict[str, list[Any]]]:
         results = []
         for image in inputs:
-            image = generalPipeline(image)
+            # image = generalPipeline(image)
             horizontal_boxes, free_boxes = self.model.detect(image)
             outputs = self.model.recognize(image, horizontal_boxes[0], free_boxes[0])
 
@@ -56,7 +56,7 @@ class EasyOCRInitedCustom(OCR):
     def __call__(self, inputs, *args, **kwargs) -> list[dict[str, list[Any]]]:
         results = []
         for image in inputs:
-            image = generalPipeline(image)
+            # image = generalPipeline(image)
             horizontal_boxes, free_boxes = self.model.detect(image)
             outputs = self.model.recognize(image, horizontal_boxes[0], free_boxes[0])
 
