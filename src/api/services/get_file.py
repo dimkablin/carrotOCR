9de69dcf76b31e0d91bcc23e54fbff1280ee2090
dimka <dmitryellison@gmail.com>
2023-11-02 23:01:19 +0300

    fixed get_file_service

--- src/api/services/get_file.py
+++ src/api/services/get_file.py
@@ -7,9 +7,7 @@ from src.utils.utils import get_abspath
 async def get_file_service(uid: int):
     """get file service's main function."""
     data = ProcessedManager.get_data_by_id(uid)
-    chunk_id = data.chunk_id
-    old_filename = data.old_filename
-    path = get_abspath("LOCAL_DATA", chunk_id, old_filename)
+    path = get_abspath("LOCAL_DATA", str(data.chunk_id), data.old_filename)
 
     if os.path.exists(path):
         return path
