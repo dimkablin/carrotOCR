b5b7ef187b14156a910b49614ec124721daa9682
Stepan <stepan.sharifulin@mail.ru>
2023-11-03 13:22:19 +0500

    Set return path

--- src/api/services/get_file.py
+++ src/api/services/get_file.py
@@ -7,7 +7,8 @@ from src.utils.utils import get_abspath
 async def get_file_service(uid: int):
     """get file service's main function."""
     data = ProcessedManager.get_data_by_id(uid)
+    server_path = "http://213.171.5.243/api/"
     path = get_abspath("LOCAL_DATA", str(data.chunk_id), data.old_filename)
 
     if os.path.exists(path):
-        return path
+        return server_path+'/'.join(path.split('/')[-3:None])
