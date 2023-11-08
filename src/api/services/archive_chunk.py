b6de76eb13f02bddde4df292ffa3f3c644bf7796
Stepan <stepan.sharifulin@mail.ru>
2023-11-09 00:21:27 +0500

    commit archive

--- src/api/services/archive_chunk.py
+++ src/api/services/archive_chunk.py
@@ -10,32 +10,32 @@ from src.db.processed_manager import ProcessedManager
 from src.db.processed_structure import ProcessedStructure
 
 
-async def archive_chunk_service(chunk_id: int,
-                                filename: str = "DATA") -> Optional[str]:
-    """archive_chunk_service function service."""
+# async def archive_chunk_service(chunk_id: int,
+#                                 filename: str = "DATA") -> Optional[str]:
+#     """archive_chunk_service function service."""
 
-    path = get_abspath("LOCAL_DATA", str(chunk_id), "original")
-    archive_path = get_abspath("LOCAL_DATA", str(chunk_id), filename + ".zip")
+#     path = get_abspath("LOCAL_DATA", str(chunk_id), "original")
+#     archive_path = get_abspath("LOCAL_DATA", str(chunk_id), filename + ".zip")
 
-    if not os.path.exists(path):
-        return None
+#     if not os.path.exists(path):
+#         return None
 
-    datas = ProcessedManager.get_data_by_chunk_id(chunk_id)
-    with zipfile.ZipFile(archive_path, "w", zipfile.ZIP_DEFLATED) as zip_file:
-        for data in datas:
-            data = ProcessedStructure().from_db(data)
+#     datas = ProcessedManager.get_data_by_chunk_id(chunk_id)
+#     with zipfile.ZipFile(archive_path, "w", zipfile.ZIP_DEFLATED) as zip_file:
+#         for data in datas:
+#             data = ProcessedStructure().from_db(data)
 
-            if data.new_filename is None:
-                warnings.warn("No new filename for data: " + str(data)[:100])
-                new_filename = data.old_filename
-            else:
-                new_filename = data.new_filename + "." + data.old_filename.split(".")[-1]
+#             if data.new_filename is None:
+#                 warnings.warn("No new filename for data: " + str(data)[:100])
+#                 new_filename = data.old_filename
+#             else:
+#                 new_filename = data.new_filename + "." + data.old_filename.split(".")[-1]
 
-            old_path = get_abspath("LOCAL_DATA", str(chunk_id), "original", data.old_filename)
-            if not os.path.exists(old_path):
-                warnings.warn(f"File {old_path} not found.")
-                continue
+#             old_path = get_abspath("LOCAL_DATA", str(chunk_id), "original", data.old_filename)
+#             if not os.path.exists(old_path):
+#                 warnings.warn(f"File {old_path} not found.")
+#                 continue
 
-            zip_file.write(old_path, arcname=new_filename)
+#             zip_file.write(old_path, arcname=new_filename)
 
-    return SERVER_PATH + archive_path[path.find('LOCAL_DATA'):]
+#     return SERVER_PATH + archive_path[path.find('LOCAL_DATA'):]
