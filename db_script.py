d7e2792a2b388107283748eedfda51c63fb42a2c
Nigel <nailkakun@mail.ru>
2023-11-14 01:00:00 +0300

    add_tags update

--- db_script.py
+++ db_script.py
@@ -1,6 +1,9 @@
 """ Script that you can run to set project env """
 from src.db.processed_manager import ProcessedManager
+from src.db.permatags_manager import PermatagsManager
 
 if __name__ == "__main__":
     if ProcessedManager.create_table():
         print(f"Table '{ProcessedManager.table_name}' created successfully.")
+    if PermatagsManager.create_table():
+        print(f"Table '{PermatagsManager.table_name}' created successfully.")
--- src/models/find_tags.py
+++ src/models/find_tags.py
@@ -1,48 +1,10 @@
 """ Find Tags model """
 import Levenshtein
 import json
-
-# class FindTags:
-#     """ Zero Shot Classification init class"""
-#     def __init__(self):
-#         self.path_to_classes = "models/zero-shot-classification/classes.txt"
-#         self.classes = []
-#         self.read_classes(self.path_to_classes)
+from typing import Optional, List, Union
 
 
-#     def read_classes(self, path) -> None:
-#         """ Read text classes from file
-#         :param path: Path to the file with classes that seperated with new line
-#         :return: None
-#         """
-#         with open(path, 'r', encoding='utf-8') as file:
-#             classes = file.readlines()
-
-#         self.classes = [cls.rstrip('\n') for cls in classes]
-
-
-#     def __call__(self, n_out: int, texts: list[str]) -> list[str]:
-#         """Find n: number classes that are closest to the text.
-
-#        :param n_out: Number of returning classes.
-#        :param texts: List of text strings.
-#        :return: The n classes closest to the text.
-#        """
-
-#         if not self.classes or not texts:
-#             return []
-
-#         class_distances = {}
-
-#         for cls in self.classes:
-#             for text in texts:
-#                 distance = Levenshtein.distance(cls, text)
-#                 if cls not in class_distances or distance < class_distances[cls]:
-#                     class_distances[cls] = distance
-
-#         sorted_classes = sorted(self.classes, key=lambda cls: class_distances[cls])
-#         return sorted_classes[:min(n_out, len(self.classes))]
-
+from src.db.permatags_manager import PermatagsManager, PermatagsStructure
 
 
 class FindTags:
@@ -67,6 +29,26 @@ class FindTags:
             self.scores = {k:0 for k in range(len(self.values))}
 
 
+    def add_perma_tags(self, tags: list[str]) -> None:
+        """ Add tags to the database"""
+        for tag in tags:
+            data = PermatagsStructure(
+                tag=tag
+            )
+            PermatagsManager.insert_data(data)
+
+
+    def rem_perma_tag(self, tag: str) -> bool:
+        """ Remove tags from the database"""
+        return PermatagsManager.delete_data_by_tag(tag)
+
+
+    def get_perma_tags(self) -> Optional[tuple]:
+        """ Get tags from the database"""
+        data = PermatagsManager.get_all_data()
+        return data
+
+
     def __call__(self, n_out: int, texts: list[str]) -> list[str]:
         """Find n: number classes that are closest to the text.
 
@@ -87,8 +69,8 @@ class FindTags:
                 self.scores[index] += score
         
         result = []
-        for id, value in self.scores.items():
+        for bbox_id, value in self.scores.items():
             if value > 0:
-                result.append(self.keys[id])
+                result.append(self.keys[bbox_id])
 
         return result
